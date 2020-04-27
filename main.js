7ef8239faee098695b823371762303ad7fc679c5
Your Name <you@example.com>
2020-04-27 15:14:55 -0400

    i don't understsnd

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 const fs = require('fs')
+
+fs.readFile('./users.json', function(error, data) {
+    if (error) {
+throw error;
+    }
+
+    const actualObj = JSON.parse(data);
+    console.log(actualObj)
+})
+// console.log(process.argv)
+
+{
+    "GET users" : [{
+        
+    }
+
+    ]
+}
