caff33064d684e2b5270fc4a599927cdc1b036d5
txddra <59891194+txddra@users.noreply.github.com>
2020-04-29 10:53:52 -0400

    Update main.js

--- main.js
+++ main.js
@@ -10,10 +10,37 @@ throw error;
 })
 // console.log(process.argv)
 
-{
-    "GET users" : [{
+// {
+//     "GET users" : [{
         
+//     }
+
+//     ]
+// }
+
+
+const postFriend = (parameters) => {
+  const [stringI, name] = parameters;
+  const i = Number(stringI);
+  fs.readFile('./users.json', (error, data) => {
+    if (error) {
+      throw error;
     }
 
-    ]
+    const users = JSON.parse(data);
+    const user = users.find((user) => user.index === i);
+    const newFriend = {
+      name,
+      index: user.friends.length,
+    }
+
+    user.friends.push(newFriend);
+    const updatedUsers = JSON.stringify(users, null, 2);
+    fs.writeFile('./users.json', updatedUsers, (error) => {
+      if (error) {
+        throw error;
+      }
+    });
+  });
 }
+
